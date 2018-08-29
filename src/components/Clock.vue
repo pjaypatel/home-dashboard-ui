dc2757a057e46b6668f7ef3aff43058207f272f2
pjpatel12 <pjpatel@knights.ucf.edu>
2018-08-28 23:23:46 -0400

    added clock component and messing around with flex box and containers

---
+++ src/components/Clock.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-flex xs6>
+    <v-card dark color="primary">
+      <v-card-text class="px-0">
+        <h1>Clock</h1>
+      </v-card-text>
+    </v-card>
+  </v-flex>
+</template>
+<script>
+export default {
+  name: 'Clock'
+}
+</script>
+<style>
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-content>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs6>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">
+              <h1>Hello World</h1>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">
+              <h1>Hello World</h1>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <Clock>
+        </Clock>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+<script>
+import Clock from './Clock.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Clock
+  }
+}
+</script>
+<style>
+</style>

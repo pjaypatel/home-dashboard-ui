acce73ffce42b5a38d9f7b994ca113f07400984a
pjpatel12 <pjpatel@knights.ucf.edu>
2018-09-01 23:48:34 -0400

    added Music player buttons and fixed clock to flexbox

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,36 +1,48 @@
 <template>
-  <v-content>
-    <v-container>
-      <v-layout row wrap>
-        <v-flex xs6>
-          <v-card dark color="primary">
-            <v-card-text class="px-0">
-              <h1>Hello World</h1>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs6>
-          <v-card dark color="primary">
-            <v-card-text class="px-0">
-              <h1>Hello World</h1>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-        <Clock>
+  <v-container>
+    <v-layout>
+      <v-flex xs2>
+        <Clock :blink='false' :displaySeconds='true'>
         </Clock>
-      </v-layout>
-    </v-container>
-  </v-content>
+      </v-flex>
+      <v-flex xs2>
+        Hello World
+      </v-flex>
+      <v-flex xs2>
+        <Music>
+        </Music>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
-import Clock from './Clock.vue'
+import Clock from 'vue-digital-clock'
+import Music from './Music.vue'
 
 export default {
   name: 'Dashboard',
   components: {
-    Clock
+    Clock,
+    Music
+  },
+  data: function () {
+    return {
+      counter: 0
+    }
   }
 }
 </script>
 <style>
+.flex {
+  background-color: #ffffff;
+}
+.Clock {
+    background-color: #000000;
+    color: #ffffff;
+    margin-left: 2px;
+    margin-right: 2px;
+    padding-left: 2px;
+    padding-right: 2px;
+    font-size: 20px;
+}
 </style>
---
+++ src/components/Music.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button v-on:click="playing=true">
+      Play
+    </button>
+    <div class="margin"/>
+    <button v-on:click="playing=false">
+      Pause
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Music",
+  data: function () {
+    return {
+      playing: false
+    }
+  }
+}
+</script>
+<style>
+.margin {
+  margin: 2px
+}
+</style>
